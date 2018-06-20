56f9e74291f415435dac16fa39907cd3d734e63f
Markus Behm <markus@ipbc.io>
2018-06-20 18:13:42 +0200

    Changes for BitTube coin and daemon

--- README.md
+++ README.md
@@ -223,7 +223,7 @@ xmrblocks, Onion Monero Blockchain Explorer:
                                         functionality
   --ssl-key-file arg                    path to key file for ssl (https)
                                         functionality
-  -d [ --deamon-url ] arg (=http:://127.0.0.1:18081)
+  -d [ --deamon-url ] arg (=http:://127.0.0.1:24182)
                                         Monero deamon url
 ```
 
--- src/templates/js/config.js
+++ src/templates/js/config.js
@@ -1,26 +1,26 @@
 var config = {
     testnet: false, // this is adjusted page.h if needed. dont need to change manually
     stagenet: false, // this is adjusted page.h if needed. dont need to change manually
-    coinUnitPlaces: 12,
+    coinUnitPlaces: 8,
     txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
-    txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-    coinSymbol: 'XMR',
-    openAliasPrefix: "xmr",
-    coinName: 'Monero',
-    coinUriPrefix: 'monero:',
-    addressPrefix: 18,
-    integratedAddressPrefix: 19,
-    subAddressPrefix: 42,
-    addressPrefixTestnet: 53,
-    integratedAddressPrefixTestnet: 54,
-    subAddressPrefixTestnet: 63,
-    addressPrefixStagenet: 24,
-    integratedAddressPrefixStagenet: 25,
-    subAddressPrefixStagenet: 36,
-    feePerKB: new JSBigInt('2000000000'),//20^10 - for testnet its not used, as fee is dynamic.
-    dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+    txCoinbaseMinConfirms: 10, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
+    coinSymbol: 'TUBE',
+    openAliasPrefix: "tube",
+    coinName: 'BitTube',
+    coinUriPrefix: 'bittube:',
+    addressPrefix: 0xd1,
+    integratedAddressPrefix: 0x404f,
+    subAddressPrefix: 0x3750,
+    addressPrefixTestnet: 0xd1,
+    integratedAddressPrefixTestnet: 0x404f,
+    subAddressPrefixTestnet: 0x3750,
+    addressPrefixStagenet: 0xd1,
+    integratedAddressPrefixStagenet: 0x404f,
+    subAddressPrefixStagenet: 0x3750,
+    feePerKB: new JSBigInt('100000'),//20^10 - for testnet its not used, as fee is dynamic.
+    dustThreshold: new JSBigInt('10000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
     txChargeRatio: 0.5,
-    defaultMixin: 4, // minimum mixin for hardfork v5
+    defaultMixin: 6, // minimum mixin for hardfork v4
     txChargeAddress: '',
     idleTimeout: 30,
     idleWarningDuration: 20,
